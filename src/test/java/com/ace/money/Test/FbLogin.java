7e433fed106e56b5aa4a864782a425aa1810fbf2
Prabhurbvn <prabhurbvn@gmail.com>
2020-10-08 10:43:21 +0530

    my first commit

---
+++ src/test/java/com/ace/money/Test/FbLogin.java
@@ -0,0 +1,45 @@
+package com.ace.money.Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class FbLogin 
+{
+	WebDriver driver;
+	@BeforeTest
+	public void setUpChrome()
+	{
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\sairam\\Downloads\\chromedriver_win32 (3)\\chromedriver.exe");
+		
+		//WebDriverManager.chromedriver().setup();
+		driver=new ChromeDriver();
+		driver.get("http://www.fb.com");
+		driver.manage().window().maximize();
+	}
+	
+	@Test
+	public void verifyTitle()
+	{
+		Assert.assertEquals(driver.getTitle(), "Facebook – log in or sign up");
+	}
+	
+	@Test
+	public void verifyUrl()
+	{
+		Assert.assertEquals(driver.getCurrentUrl(), "https://www.facebook.com/");
+	}
+	
+	@AfterTest
+	public void tearDown()
+	{
+		driver.close();
+	}
+
+}
